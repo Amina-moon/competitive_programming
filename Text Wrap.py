7b33c07e64a7e5dd2a9836d2d58ec2b1b39e04c2
AYA <118719938+Amina-moon@users.noreply.github.com>
2023-10-12 21:50:06 +0300

    Create Text Wrap.py

---
+++ Text Wrap.py
@@ -0,0 +1,11 @@
+import textwrap
+
+def wrap(string, max_width):
+    wrapped=textwrap.wrap(string,max_width)
+    wrapped_string = '\n'.join(wrapped)
+    return wrapped_string
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
